2101056496f7ff9d3e315f0602d160e972fc1efb
Jin Dhaliwal <jin.dhaliwal@teknion.com>
2017-02-27 16:52:23 -0500

    fix devise flows

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -35,8 +35,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in_and_redirect @user
     else
+      session["omniauth_strategy"] = strategy
       session["devise.#{strategy}_data"] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url(omniauth_strategy: strategy)
+      redirect_to new_user_registration_url
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,7 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      binding.pry
-      if data = session["devise.#{params[:omniauth_strategy]}_data"] && session["devise.#{params[:omniauth_strategy]}_data"]["extra"]["raw_info"]
+      if data = session["devise.#{session[:omniauth_strategy]}_data"] && session["devise.#{session[:omniauth_strategy]}_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
     end
